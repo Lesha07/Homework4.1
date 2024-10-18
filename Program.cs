16421bf2d384b12e0fa08f85b15c209b6117aadd
Lesha07 <simchuklesha@gmail.com>
2024-10-18 22:05:22 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите день (1-7)");
+byte number = byte.Parse(Console.ReadLine());
+switch (number)
+{
+    case 1:
+    Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Вториник");
+        break;
+    case 3:
+        Console.WriteLine("Среда");
+        break;
+    case 4:
+        Console.WriteLine("Четверг");
+        break;
+    case 5:
+        Console.WriteLine("Пятница");
+        break;
+    case 6:
+        Console.WriteLine("Суббота");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье");
+        break;
+    default:
+        Console.WriteLine("error number");
+        break;
+}
